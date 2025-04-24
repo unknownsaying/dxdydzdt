9132f25ec9561b87f19a087b276ee7c7af06730f
unknownsaying <15368104568@189.cn>
2025-04-24 19:37:52 +0800

    Add files via upload

---
+++ quark.vb
@@ -0,0 +1,11 @@
+Enum Quark
+const c = 299792458
+const c^2 = 299792458*299792458
+name : {Bottom Charm Down Strange Top Up}
+mass : [4180 MeV/c^2 & 1275 MeV/c^2 & 4.8 MeV/c^2 & 95 MeV/c^2 & 173210 MeV/c^2 & 2.3 MeV/c^2]
+momentum : < & & & & & >
+charge : ( -1/3 | 2/3 | -1/3 | -1/3 | 2/3 | 2/3 )
+mass-charge-scale : < | | | | | > 
+spin : [1/2 = 1/2 = 1/2 = 1/2 = 1/2 = 1/2 ]
+color : || red && green && blue|| 
+End Enum
