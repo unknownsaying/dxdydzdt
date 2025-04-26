6d0707dfc736b2fcdf2710190dddb1a76d63db32
unknownsaying <15368104568@189.cn>
2025-04-26 13:10:05 +0800

    Update quark.vb

--- quark.vb
+++ quark.vb
@@ -5,7 +5,8 @@ name : {Bottom Charm Down Strange Top Up}
 mass : [4180 MeV/c^2 & 1275 MeV/c^2 & 4.8 MeV/c^2 & 95 MeV/c^2 & 173210 MeV/c^2 & 2.3 MeV/c^2]
 momentum : < & & & & & >
 charge : ( -1/3 | 2/3 | -1/3 | -1/3 | 2/3 | 2/3 )
-mass-charge-scale : < | | | | | > 
+mass-charge-scale : ( | | | | | )
 spin : [1/2 = 1/2 = 1/2 = 1/2 = 1/2 = 1/2 ]
-color : || red && green && blue|| 
+mass-spin-propability : [ = = = = = = ]
+color : || red && green && blue || 
 End Enum
